90e06ddeb38f442ff443cb84cacdd57663663f88
Hercrat <l21070074@gmail.com>
2025-02-26 21:49:11 -0600

    Avances hasta el video 13.2

--- backend/config/env.js
+++ backend/config/env.js
@@ -1,6 +1,6 @@
 function env_() {
     return{
-        ID_DATABASE:"1Kdbbt23pNN3-DdPQtIMtopW4SNMrvYuhYLRPZMLOCE4",
+        ID_DATABASE:"1kOZMD6enpzDcatkpVVvAPA1wSCoCS_iiIdHMqmS9XcU",
         SH_REGISTRO_USUARIOS:"Registro Usuarios",
     };
 }
